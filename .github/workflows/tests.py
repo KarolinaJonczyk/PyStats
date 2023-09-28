8c25950d912b3da64fea67f803bd7f3b1039f42b
Karolina Jonczyk <253737@student.pwr.edu.pl>
2023-09-28 13:49:22 +0200

    update tests.py
    2 asserts for method skewness_coef

--- .github/workflows/tests.py
+++ .github/workflows/tests.py
@@ -52,3 +52,6 @@ assert statistic_range([1,2,3,4]) == 4
 
 assert average_absolute_deviation([1,2,3]) == 0.666666
 assert average_absolute_deviation([1,2,3]) == 0.6666666666666666
+
+assert skewness_coef([1,1,2]) == 1
+assert skewness_coef([1,1,2,2]) == 1
