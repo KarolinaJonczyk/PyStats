0692fb56541d06352fa5b44230c0bd953a7e68e6
Karolina Jonczyk <253737@student.pwr.edu.pl>
2023-09-30 22:26:01 +0200

    add new methods

--- src/Pystats1.py
+++ src/Pystats1.py
@@ -19,7 +19,7 @@ def median(x:list)->float:
         return TypeError("mean not exist")
     if n%2 == 1:
         return x[n//2]
-    if n%2 != 1:
+    else:
         return (x[n//2] + x[n//2 - 1])/2
     
 def low_median(x:list)->float:
@@ -32,7 +32,7 @@ def low_median(x:list)->float:
         return TypeError("mean not exist")
     if n%2 == 1:
         return x[n//2]
-    if n%2 != 1:
+    else:
         return x[n//2 - 1]
     
 def high_median(x:list)->float:
@@ -61,8 +61,7 @@ def standard_deviation(x:list)->float:
     """returns standard deviation of numeric data
     find the square root of the average
     of the squared differences between each 
-    data point and the mean
-    """
+    data point and the mean"""
     mean_x = mean(x)
     n = len(x)
     s=0
@@ -101,7 +100,7 @@ def quantiles(q_n, x:list)->float:
         if q_n == 4:
             return max(x)
 
-def geom_mean(x:list)->float:
+def geometric_mean(x:list)->float:
     '''Geometric mean is defined as the n-th root of the product of n numbers'''
     n = len(x)
     result = 1
@@ -129,13 +128,12 @@ def generalized_mean(k, x:list)->float:
     n = len(x)
     if k == 0:
         result=geom_mean(x)
-        return result
     else:
         result = 0
         for i in range(n):
             result += x[i]**k
         result = (result/n)**k
-        return result
+    return result
 
 def mode(x:list)->list:
     '''Returns the most frequent element in the list 
@@ -149,8 +147,7 @@ def mode(x:list)->list:
         elements = list(set(x))
     for i, count in enumerate(counting):
         if count == maximum:
-            mode.append(elements[i]
-
+            mode.append(elements[i])
     return mode
 
 def statistic_range(x:list)->float:
@@ -170,6 +167,76 @@ def average_absolute_deviation(x:list)->float:
         s += abs(x[i] - xmean)
     avg_abs_mean = s/n
     return avg_abs_mean
+
+def skewness_coef(x:list)->float:
+    '''skewnes coefficient is is a measure of the asymmetry of the probability 
+    distribution of a real-valued random variable about its mean.
+    The skewness value can be positive, zero, negative, or undefined.
+    negative skew - left-skewed
+    zero - no skewness
+    positive skew - right-skewed'''
+    if len(mode(x)) == 1:
+        skew = (mean(x) - mode(x)[0])/standard_deviation(x)
+    else:
+        skew = (mean(x) - mean(mode(x))) / standard_deviation(x)
+    return skew
+
+def third_central_moment(x:list)->float:
+    '''according to the definition of the central moment, the sum of 
+    the third powers of deviations of the value of a statistical 
+    feature from the arithmetic mean value, divided by n
+    zero - symmetrical distribution
+    negative - left-skewed
+    positive - right-skewed'''
+    n = len(x)
+    m = mean(x)
+    s = 0
+    for i in range(n):
+        s += (x[i] - m)**3
+    s = s/n
+    return s
+
+def kurtosis(x:list)->float:
+    '''kurtosis is a measure of the "tailedness" of the probability
+    distribution of a real-valued random variable.
+    zero -  the excess kurtosis of any univariate normal distribution
+    positive - leptokurtic
+    negative - platykurtic'''
+    n = len(x)
+    s = 0
+    for i in range(n):
+        s+=(1/n) * (x[i] - mean(x))**4
+    return s/standard_deviation(x)
+
+def excess_kurtosis(x:list)->float:
+    '''The excess kurtosis is defined as kurtosis minus 3.
+    '''
+    xkurtosis = kurtosis(x)
+    ex_kurtosis = xkurtosis - 3
+    return ex_kurtosis
+
+def gini_coefficient(x:list)->float:
+    '''work in progress...
+    I need to read more about this method'''
+    n = len(x)
+    if x == sorted(x):
+        s = 0
+        for i in range(n):
+            s += (2*i - n -1)*x[i]
+        gini = s/(mean(x)*n**2)
+    #else/elif:...
+        return gini   
+
+def covariance(x:list, y:list)->float:
+    '''Covariance is a measure of the joint variability of two random variables.'''
+    n = len(x)
+    xmean = mean(x)
+    ymean = mean(y)
+    cov = 0
+    for i in range(n):
+        cov += (x[i] - xmean)*(y[i] - ymean)
+    cov = cov/n
+    return cov
 
 def testfunc():
     pass
